278f215c0f3bbb93e9bd999457ab9395e3de0807
golden lira <empirecheat17@gmail.com>
2024-02-21 00:50:40 +0300

    Create Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿bool Palindrom(string str)
+{
+    string temp = string.Empty;
+    for ( int i = str.Length - 1; i >= 0; i--)
+    {
+        temp += str[i];
+    }
+    if (temp == str)
+    {
+        return true;
+    }
+    return false;
+}
+
+Console.Write("Введите текст: ");
+string TextFromConsole = Console.ReadLine();
+bool Paldrom = Palindrom(TextFromConsole);
+if (Paldrom)
+{
+    Console.WriteLine($"Строка является палиндромом");
+}
+else 
+{
+    Console.WriteLine($"Строка не является палиндромом");
+}
