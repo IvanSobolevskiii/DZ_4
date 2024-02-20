ceed82eb219c7455d9a6ca5e2ff98f6302756464
golden lira <empirecheat17@gmail.com>
2024-02-20 22:26:54 +0300

    Create Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,6 @@
+﻿string str = "QuIte a sTraNgE TaSk";
+
+Console.Clear();
+Console.WriteLine($"\nИсходная строка: {str}\n");
+Console.WriteLine($"Строка в нижнем регистре: {str.ToLower()}\n");
+Console.WriteLine($"Строка в верхнем регистре: {str.ToUpper()}\n");
